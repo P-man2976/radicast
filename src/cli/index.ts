c1cde3be28b62f6b0960656d2d63bdca835666c6
ぴーまん <ikmy1119924@gmail.com>
2024-02-21 23:04:54 +0900

    v1.0.5: Add CORS option

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,6 +2,7 @@
 
 import { networkInterfaces } from "os";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
 import { serve } from "@hono/node-server";
 import yargs from "yargs";
@@ -12,19 +13,30 @@ import radiruRoute from "./radiru.js";
 
 console.log("[Server] Starting radicast server...");
 
-const parser = yargs(hideBin(process.argv)).option("port", {
-	alias: "p",
-	default: 8080,
-	description: "HTTP server port number",
-	type: "number",
-});
+const parser = yargs(hideBin(process.argv))
+	.option("port", {
+		alias: "p",
+		default: 8080,
+		description: "HTTP server port number",
+		type: "number",
+	})
+	.option("cors", {
+		alias: "c",
+		default: false,
+		description: "Add `Access-Control-Allow-Origin` Header",
+		type: "boolean",
+	});
+
 export const port = parser.parseSync().port || 8080;
+const isCorsEnabled = parser.parseSync().cors;
 
 const netInfos = networkInterfaces();
 const interfaces = Object.values(netInfos).flatMap((val) => val ?? []);
 export const address = interfaces?.find(({ family }) => family === "IPv4")?.address ?? "127.0.0.1";
 
 const app = new Hono();
+
+if (isCorsEnabled) app.use(cors());
 
 app.onError(async (err, c) => {
 	if (err instanceof HTTPException) {
